68b0a758ea7453577e99718d71e2f48a49954e9b
Coco <sandra_pimenta@gmx.com>
2018-12-11 16:54:37 +0100

    Suppression des lieux

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route; // Définir les routes en annotations
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
+use App\Form\LieuType;
 use App\Entity\Lieu;
 
 /**
@@ -35,5 +41,78 @@ class LieuController extends AbstractController
         return $this->render('/lieu/show.html.twig', array(
             'entity' => $entity,
         ));
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request, EventDispatcherInterface $eventDispatcher)
+    {
+        $entity = new Lieu;
+
+        $form = $this->createForm(LieuType::class, $entity);
+        $form->handleRequest($request); // Envoi les données de requêtes (POST) au formulaire
+
+        if ($form->isSubmitted() && $form->isValid()) { // Si le form est envoyé et valide
+            // Ajout du nouvel lieu dans la db
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity); // Prépare la requête
+            $em->flush(); // Execute la requête
+
+            // Crée un message de confirmation
+            $t = $this->get('translator');
+            $this->addFlash('success', $t->trans('lieu.new.success'));
+
+            // Déclanchement de l'événement 'lieu.add'
+            $event = new GenericEvent($entity);
+            $eventDispatcher->dispatch('lieu.add', $event);
+
+            return $this->redirectToRoute('lieu_index'); // Retour sur la liste des lieux
+        }
+
+        return $this->render('lieu/new.html.twig', array(
+            'form' => $form->createView(), // Envoi le formulaire à la vue
+        ));
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", requirements={"id" = "\d+"})
+     */
+    public function edit(Request $request, Lieu $entity)
+    {
+        $form = $this->createForm(LieuType::class, $entity);
+        $form->handleRequest($request); // Envoi les données de requêtes (POST) au formulaire
+
+        if ($form->isSubmitted() && $form->isValid()) { // Si le form est envoyé et valide
+            // Ajout du nouvel lieu dans la db
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity); // Prépare la requête
+            $em->flush(); // Execute la requête
+
+            // Crée un message de confirmation
+            $t = $this->get('translator');
+            $this->addFlash('success', $t->trans('lieu.edit.success'));
+
+            return $this->redirectToRoute('lieu_index'); // Retour sur la liste des lieux
+        }
+
+        return $this->render('lieu/edit.html.twig', array(
+            'form' => $form->createView(), // Envoi le formulaire à la vue
+            'entity' => $entity,
+        ));
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", requirements={"id" = "\d+"})
+     */
+    public function delete(Request $request, Lieu $lieu): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$lieu->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($lieu);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('lieu_index');
     }
 }
