17210dbca207ffd3fa802f0c347784094587f741
Zuryka <vega.christopher51@gmail.com>
2018-12-12 12:14:56 +0100

    commit ajout parti "mon compte"

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Form\Admin\UserType;
+//use App\Form\Admin\UserType as AdminUserType;
+use App\Form\UserType;
 use App\Entity\User;
 
 /**
